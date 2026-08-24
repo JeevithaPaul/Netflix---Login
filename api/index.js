503fb5139d1eb32c4df519faab0e5e89ce6a518a
Jeevitha Paul <pauljeevi23@gmail.com>
2026-08-25 00:09:08 +0100

    Fix Vercel login API

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Netflix Login Backend is running!");
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  const validEmail = "test@gmail.com";
+  const validPassword = "123456";
+
+  if (email === validEmail && password === validPassword) {
+    res.json({
+      success: true,
+      message: "Login successful",
+    });
+  } else {
+    res.status(401).json({
+      success: false,
+      message: "Invalid email or password",
+    });
+  }
+});
+
+module.exports = app;
