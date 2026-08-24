1ef7e931e6330e8abb7916f6dae07d5646d013c1
Jeevitha Paul <pauljeevi23@gmail.com>
2026-08-24 23:09:10 +0100

    Update login API for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
